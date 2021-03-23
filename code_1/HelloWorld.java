5bd96a0611780d09038b3fad8a04c01260229ecf
LusionL <55376007+LusionL@users.noreply.github.com>
2021-03-23 16:38:38 +0800

    Add files via upload

---
+++ code_1/HelloWorld.java
@@ -0,0 +1,76 @@
+package code_1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		//float c = 20;
+		//int c = '1';
+		//System.out.println("Hello World!");
+		//System.out.println("nihao!\n\'Helloworld\'");
+		//long c = 5;
+		//double c = 5.0;
+		
+//		int a = 65;
+//		char c;
+//		c = (char)a;
+//		System.out.println(c);
+		
+		
+//		String a;
+//		a = "ab" + "cde";
+//		System.out.println(a);
+		
+		
+//		String a;
+//		a = "a:" + 4*5;
+//		System.out.println(a);
+		
+		
+//		float a = 23.5f;
+//		System.out.println(a);
+		
+//		int b=10;
+//		int a = 1;
+//		a*= b + 5;
+//		System.out.println(a);
+		
+//		double a = 1000.123321;
+//		int b = (int)a;
+//		System.out.println(b);//精度丢失
+		
+//		int a = 1;
+//		int num = 10;
+//		int result = a==0?100:2*num;
+//		System.out.println(result);
+		
+		
+//		int a =0;
+//		int b =0;
+//		boolean c = a==0&&b==0;
+//		System.out.println(c);
+		
+//		int a = 0;
+//		int b =1;
+//		int c = a==0&&b==2?1:2;
+//		System.out.println(c);
+		
+		
+//		int i = 10;
+//		int j = 10;
+//		int a = 1;
+//		int b = 2;
+//		int c = 3;
+//		int d = 4;
+//		int k = 200;
+//		
+//		if(i==10) {
+//			if(j<20)
+//				a=b;
+//			if(k>100)
+//				c=d;
+//			else a=c;
+//		}else
+//			a=d;
+//		System.out.println(a +","+c); //通过 a + "," + b 来同时输出两个变量的值
+
+	}
+}
